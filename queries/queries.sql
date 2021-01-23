b2c2fc93f6d3eacb506b5d9044d32b428356f706
SoWhitIs <73681432+SoWhitIs@users.noreply.github.com>
2021-01-23 06:12:47 -0500

    update script_retirement

---
+++ queries/queries.sql
@@ -0,0 +1,37 @@
+--Retirement eligibility, BIRTH DATE: January 1, 1952, and December 31, 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+--Retirement eligibility, BIRTH DATE: 1952
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+--Retirement eligibility, BIRTH DATE: 1953
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+--Retirement eligibility, BIRTH DATE: 1954
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+--Retirement eligibility, BIRTH DATE: 1955
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility, BIRTH DATE: January 1, 1952, and December 31, 1955, HIRED: 1985 and 1988
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Count Queries
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
