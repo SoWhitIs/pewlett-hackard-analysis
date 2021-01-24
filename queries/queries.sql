6efef9039e33bed9973c5838ff62f844bc3cc61c
SoWhitIs <73681432+SoWhitIs@users.noreply.github.com>
2021-01-24 04:27:08 -0500

    updated scripts--additional lists

--- queries/queries.sql
+++ queries/queries.sql
@@ -120,3 +120,107 @@ GROUP BY de.dept_no
 ORDER BY de.dept_no;
 -- Check the table
 SELECT * FROM retirement_dept;
+
+
+--Create Employee Information List
+--Review Salaries Table
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+-- 
+SELECT e.emp_no,
+    e.first_name,
+e.last_name,
+    e.gender,
+    s.salary,
+    de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+     AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	  AND (de.to_date = '9999-01-01');  
+		  
+-- Create Management List
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+
+-- Department Retirees List
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+-- Retirement List: Sales Department
+-- List Includes: Employee numbers,Employee first name,Employee last name, Employee department name
+-- Review retirement_info, and like tables
+SELECT * FROM dept_info;
+-- 
+SELECT * FROM retirement_info;
+-- 
+SELECT * FROM departments;
+
+--Create List, Option #1: , building from retirement_info
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name, d.dept_name
+-- INTO retirement_sales
+FROM retirement_info as ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no=de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no=d.dept_no)
+WHERE d.dept_name ='Sales';
+
+-- Option #2: Alternative to Filter By Sales, use prviously filtered dept_info table
+SELECT * FROM dept_info
+-- INTO retirement_sales
+WHERE dept_name = 'Sales';
+
+
+
+-- Retirement List: Sales and Development Teams
+-- List Includes: Employee numbers,Employee first name,Employee last name, Employee department name 
+-- Review retirement_info, and like tables
+SELECT * FROM dept_info;
+-- 
+SELECT * FROM retirement_info;
+-- 
+SELECT * FROM departments;
+
+--Create List, Option #1: , building from retirement_info
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name, d.dept_name
+-- INTO retirement_sales_dev
+FROM retirement_info as ri
+INNER JOIN dept_emp AS de
+ON (ri.emp_no=de.emp_no)
+INNER JOIN departments as d
+ON (de.dept_no=d.dept_no)
+WHERE d.dept_name IN ('Sales','Development');
+
+-- Option #2: Alternative to Filter By Sales and Development, use prviously filtered dept_info table 
+SELECT * FROM dept_info
+-- INTO retirement_sales_dev
+WHERE dept_name IN ('Sales','Development');
